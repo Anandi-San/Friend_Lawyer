495c02df137c05be95b032fddd0946fa7aa9768b
Anandi Saputra <sptraanandi@gmail.com>
2023-06-06 00:05:10 +0800

    update Backend

--- back-end/controllers/Discussion.js
+++ back-end/controllers/Discussion.js
@@ -2,72 +2,49 @@ import {Op} from "sequelize";
 import Discussion from "../models/DiscussionModel.js";
 import User from "../models/UserModel.js";
 
-export const getDiscussion = async (req , res) => {
+export const getDiscussion = async (req, res) => {
     try {
-        let response;
-        if(req.role === "admin"){
-            response = await Discussion.findAll({
-                attributes:['uuid','title','content'],
-                include:[{
-                    model: User, 
-                    attributes: ['name','email']
-                }]
-            });
-        }else{
-            response = await Discussion.findAll({
-                attributes:['uuid','title','content'],
-                where:{
-                    userId: req.userId
-                },
-                include:[{
-                    model: User,
-                    attributes:['name','email']
-                }]
-            });
-        }
-        res.status(200).json(response);
+      const response = await Discussion.findAll({
+        attributes: ['uuid', 'title', 'content'],
+        include: [{
+          model: User,
+          attributes: ['name', 'email']
+        }]
+      });
+  
+      res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({msg: error.message});
+      res.status(500).json({ msg: error.message });
     }
-}
+  };
+  
 
-export const getDiscussionById = async (req , res) => {
+  export const getDiscussionById = async (req, res) => {
     try {
-        const discussion = await Discussion.findOne({
-            where:{
-                uuid: req.params.id
-            }
-        });
-        if(!discussion) return res.status(404).json({msg: "Data tidak ditemukan"});
-        let response;
-        if(req.role === "admin"){
-            response = await Discussion.findOne({
-                attributes:['uuid','title','content'],
-                where:{
-                    id: discussion.id
-                },
-                include:[{
-                    model: User,
-                    attributes:['name','email']
-                }]
-            });
-        }else{
-            response = await Discussion.findOne({
-                attributes:['uuid','title','content'],
-                where:{
-                    [Op.and]:[{id: discussion.id}, {userId: req.userId}]
-                },
-                include:[{
-                    model: User,
-                    attributes:['name','email']
-                }]
-            });
+      const discussion = await Discussion.findOne({
+        where: {
+          uuid: req.params.id
         }
-        res.status(200).json(response);
+      });
+      if (!discussion) return res.status(404).json({ msg: "Data tidak ditemukan" });
+  
+      const response = await Discussion.findOne({
+        attributes: ['uuid', 'title', 'content'],
+        where: {
+          id: discussion.id
+        },
+        include: [{
+          model: User,
+          attributes: ['name', 'email']
+        }]
+      });
+  
+      res.status(200).json(response);
     } catch (error) {
-        res.status(500).json({msg: error.message});
+      res.status(500).json({ msg: error.message });
     }
-}
+  };
+  
     
 
 export const createDiscussion = async (req, res) => {
--- back-end/controllers/Message.js
+++ back-end/controllers/Message.js
@@ -4,40 +4,26 @@ import Discussion from "../models/DiscussionModel.js";
 
 export const getMessages = async (req, res) => {
   try {
-    let response;
-    if (req.role === "admin") {
-      response = await Message.findAll({
-        attributes: ["uuid", "pesan"],
-        include: [
-          {
-            model: User,
-            attributes: ["name", "email"],
-          },
-          {
-            model: Discussion,
-            attributes: ["title"],
-          },
-        ],
-      });
-    } else {
-      response = await Message.findAll({
-        attributes: ["uuid", "pesan"],
-        where: {
-          userId: req.userId,
+    const response = await Message.findAll({
+      attributes: ["uuid", "pesan"],
+      include: [
+        {
+          model: User,
+          attributes: ["name", "email"],
         },
-        include: [
-          {
-            model: User,
-            attributes: ["name", "email"],
-          },
-        ],
-      });
-    }
+        {
+          model: Discussion,
+          attributes: ["title"],
+        },
+      ],
+    });
+
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
+
 
 export const getMessageById = async (req, res) => {
   try {
@@ -87,79 +73,79 @@ export const getMessageById = async (req, res) => {
 
 export const getMessageByDiscussion = async (req, res) => {
   const { discussionId } = req.params;
-  // console.log(discussionId)
+
   try {
-    const message = await Message.findAll({
-      include: [{
-        model: Discussion,
-        // where: ["message.discussionId = discussion.id"],
-        required: true
-      },
-      {
-        model: User,
-        attributes: ["name"], // Mengambil atribut 'name' dari tabel 'User'
-        // as: "user" // Memberikan alias 'user' pada relasi antara Message dan User
-      }
-    ],
+    const messages = await Message.findAll({
+      include: [
+        {
+          model: Discussion,
+          required: true
+        },
+        {
+          model: User,
+          attributes: ["name"]
+        }
+      ],
       where: {
-        discussionId: discussionId,
-      },
+        discussionId: discussionId
+      }
     });
-    if (!message) return res.status(404).json({ msg: "Data tidak ditemukan" });
-    let response;
-    if (req.role === "admin") {
-      response = await Discussion.findAll({
-        include: [
-          {
+
+    if (!messages) return res.status(404).json({ msg: "Data tidak ditemukan" });
+
+    const response = await Discussion.findAll({
+      include: [
+        {
           model: Message,
           attributes: ["uuid", "pesan", "discussionId", "updatedAt"],
-          include:[{
-            model: User,
-                attributes: ["name"],
-                // as: "user"
-          }]
-        },
-      ],
-            attributes: [],
-        where: {
-          "uuid": discussionId
+          include: [
+            {
+              model: User,
+              attributes: ["name"]
+            }
+          ]
         }
-      })
+      ],
+      attributes: [],
+      where: {
+        uuid: discussionId
+      }
+    });
 
-    } else {
-      response = await Discussion.findAll({
-        attributes: ["uuid", "title", "content"],
-        where: {
-          [Op.and]: [{ discussionId: discussion.id }, { userId: req.userId }],
-        },
-        include: [
-          {
-            model: User,
-            attributes: ["name", "email"],
-          },
-        ],
-      });
-    }
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
 
+
 export const createMessage = async (req, res) => {
-  const { discussionId } = req.params;
   const { pesan } = req.body;
+  const { discussionId } = req.params;
+
   try {
+    const discussion = await Discussion.findOne({
+      where: {
+        uuid: discussionId,
+      },
+    });
+
+    if (!discussion) {
+      return res.status(404).json({ msg: "Discussion not found" });
+    }
+
     await Message.create({
       pesan: pesan,
       userId: req.userId,
-      discussionId: discussionId,
+      discussionId: discussion.id,
     });
-    res.status(201).json({ msg: "Massage Created Successfuly" });
+
+    res.status(201).json({ msg: "Message Created Successfully" });
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
 };
+
 
 export const deleteMessage = async (req, res) => {
   try {
